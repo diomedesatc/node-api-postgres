a5f2b6c004b22c64c98b9a0dd56323a1d39800ae
Diomedes Trinidad <diomedesinf39@hotmail.com>
2024-07-26 14:01:59 -0300

    Some features were not working as expected and they got fixed already

--- queries.js
+++ queries.js
@@ -10,7 +10,7 @@ const pool = new Pool({
 
 const getUsers = (req, res) =>{
     pool.query('SELECT * FROM users ORDER BY ID ASC', (error, result) =>{
-        if(error) throw error;
+        if(error) {throw error}
         res.status(200).json(result.rows);
     })
 };
@@ -18,7 +18,7 @@ const getUsers = (req, res) =>{
 const getUserById = (req, res) =>{
     const id = parseInt(req.params.id);
     pool.query('SELECT * FROM users WHERE id = $1',[id], (error,result)=>{
-        if(error) throw error;
+        if(error) {throw error}
         res.status(200).json(result.rows);
     })
 };
@@ -26,8 +26,8 @@ const getUserById = (req, res) =>{
 const createUser = (req, res) =>{
     const {name, email} = req.body;
     pool.query('INSERT INTO users(name, email) VALUES ($1,$2) RETURNING *', [name,email],(error,result) =>{
-        if(error) throw error;
-        res.status(201).json(result.rows);
+        if(error) {throw error}
+        res.status(201).send(`User added with ID: ${result.insertId}`)
     })
 };
 
@@ -38,7 +38,7 @@ const updateUser = (req, res) =>{
     pool.query(
         'UPDATE users SET name = $1, email = $2 WHERE id = $3', [name, email, id],
         (error, result) =>{
-            if(error) throw error;
+            if(error) {throw error}
             res.status(200).send(`User modified with the id of: ${id}`);
         }
     )
@@ -48,8 +48,8 @@ const updateUser = (req, res) =>{
 const deleteUser = (req, res) =>{
     const id = parseInt(req.params.id);
     pool.query('DELETE FROM users WHERE id = $1', [id] , (error, result)=>{
-        if(error) throw error;
-        result.status(200).send(`The user with the id: ${id} was sucessfully deleted`);
+        if(error) {throw error}
+        res.status(200).send(`The user with the id: ${id} was sucessfully deleted`);
     })
 };
 
